aea5efde69732dbc0c03e28b7a17213fb8a9dca7
CopperKim <copperkim29@gmail.com>
2025-08-25 02:45:00 +0900

    fix(Main) : Added transform validation pipe 	modified:   apps/server/src/main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   app.use(cookieParser()) 
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })) // request body로 들어온 데이터의 유효성 검사. DTO가 필요함
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true })) // request body로 들어온 데이터의 유효성 검사. DTO가 필요함
   // whitelist : 검증 규칙이 정의되지 않은 프로퍼티를 제거해준다 
 
   app.enableCors(settings().getCorsConfig()) // enableCors : 서로 다른 출처의 리소스 공유. 이를테면 백엔드-프론트엔드. settings에 그 주소가 저장되어 있다 
